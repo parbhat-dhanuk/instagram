b5ddc7c0b9052468dcf362277676cbf162c9364f
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-11-07 16:17:03 +0545

    get & edit profile

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -92,3 +92,52 @@ export const logout=async (_,res)=>{
         console.log(error)
     }
 }
+
+
+export const  getProfile=async (req,res)=>{
+try {
+    const userId = req.params.id;
+    let user = await User.findById(userId).populate({path:'posts', createdAt:-1}).populate('bookmarks');
+    return res.status(200).json({
+        user,
+        success: true
+    });
+} catch (error) {
+    console.log(error)
+}
+}
+
+export const editProfile=async (req,res)=>{
+    try {
+        const userId = req.id;
+        const { bio, gender } = req.body;
+        const profilePicture = req.file;
+        let cloudResponse;
+
+        if (profilePicture) {
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse = await cloudinary.uploader.upload(fileUri);
+        }
+
+        const user = await User.findById(userId).select('-password');
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found.',
+                success: false
+            });
+        };
+        if (bio) user.bio = bio;
+        if (gender) user.gender = gender;
+        if (profilePicture) user.profilePicture = cloudResponse.secure_url;
+
+        await user.save();
+
+        return res.status(200).json({
+            message: 'Profile updated.',
+            success: true,
+            user
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
